03711765d621088568dc0dd8642522a88d028a4f
aryantiwari12 <81208911+aryantiwari12@users.noreply.github.com>
2022-01-11 10:47:07 +0530

    Create switch2homework.cpp

---
+++ switch2homework.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    while (1)
+    {
+        cout << "you are work" << endl;
+        char ch = 'a';
+        switch (ch)
+        {
+        case 'a':
+            cout << "bsdk";
+            break;
+        }
+        exit(0);
+    }
+}
